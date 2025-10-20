62b9302f6cfdb7cba1ca63d14bc1cb653fc5a462
BeateDuP <u04659075@tuks.co.za>
2025-10-20 22:10:08 +0200

    Populating database

--- test_data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO "User" (Username, Email, Password, Classification)
-VALUES ('Admin1', 'admin@market.com', '12345', 'MarketAdministrator');
-
-INSERT INTO MarketAdministrator (ContactNumber, UserID)
-VALUES ('0821234567', 1);
-
-INSERT INTO Market (MarketName, MarketDescription, MarketLocation)
-VALUES ('Mzanzi Market', 'A vibrant local market.', 'Pretoria');
